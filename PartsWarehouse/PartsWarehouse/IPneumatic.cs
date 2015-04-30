f7be132f0fdbb156f262d7da47e1fac63a493a3a
DenisVashkevich <vdenis.biocom@gmail.com>
2015-04-30 11:17:41 +0300

    схема
    Схема приложения

---
+++ PartsWarehouse/PartsWarehouse/IPneumatic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PartsWarehouse
+{
+    public interface IPneumatic:IPart
+    {
+        double OperatingPressure { get; }
+        int ConnectionDiameter { get; }
+    }
+}
---
+++ PartsWarehouse/PartsWarehouse/Relay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PartsWarehouse
+{
+    public class Relay:Part,IHasACoil
+    {
+        int NumNOcont { get; set; }
+        int NumNCcont { get; set; }
+        int TotalContacts { get; }
+
+        public double CoilVoltage
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
