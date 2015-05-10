50f085a639628153ccfd9bc8f1a978cf0da9fa56
DenisVashkevich <vdenis.biocom@gmail.com>
2015-05-10 13:37:32 +0300

    begin

---
+++ FlightCompany/FlightCompany/IPlane.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FlightCompany
+{
+    public interface IPlane
+    {
+        public string Manufacturrer { get; }
+        public string Model { get; }
+        public int FlightRange { get; }
+        public int CruisingSpeed { get; }
+        public int FuelConsumption { get; }
+        public int CrewCount { get; }
+    }
+}
