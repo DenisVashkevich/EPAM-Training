6de3df24329dc2eefee25416ac473f7d1b2798c4
DenisVashkevich <vdenis.biocom@gmail.com>
2015-05-24 01:57:23 +0300

    Concordance added

---
+++ Concordance/Concordance/Classes/Concordance.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Concordance
+{
+    public class Concordance
+    {
+        private Dictionary<string, int> words = new Dictionary<string, int>();
+
+        public void Add(string wrd)
+        {
+            int val;
+
+            if(words.TryGetValue(wrd, out val))
+            {
+                words[wrd]++;
+            }
+            else
+            {
+                words.Add(wrd, 1);
+            }
+        }
+
+        public void AddRange()
+        {
+
+        }
+
+        public void DisplayWords()
+        {
+            var wrds = from ww in words orderby ww.Key ascending select ww;
+            foreach (KeyValuePair<string, int> w in wrds)
+            {
+                Console.WriteLine(w.Key + "   " + w.Value.ToString());
+            }
+        }
+    }
+}
---
+++ Concordance/Concordance/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Concordance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fp = "d:\\test.txt";
+            Concordance wwords = new Concordance();
+            try
+            {
+                using (StreamReader sr = new StreamReader(fp))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string s = sr.ReadLine();
+                        string[] sar = s.Split(new char[] {' ', ',', '.', ':', '\t' });
+                        foreach (string ss in sar)
+                        {
+                            if (ss.Trim() != "")
+                                wwords.Add(ss.ToLower());
+                        }
+                    }
+                    
+                }
+            }
+            catch(FileNotFoundException ex) 
+            { 
+            }
+
+            wwords.DisplayWords();
+            Console.ReadLine();
+        }
+    }
+}
