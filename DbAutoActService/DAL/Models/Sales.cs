583655bbf0aa02e3db0d26f272fc466a274bc96f
DenisVashkevich <vdenis.biocom@gmail.com>
2015-06-27 14:49:41 +0300

    database model added

---
+++ DbAutoActService/DAL/Models/Sales.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class Sales
+    {
+        public DateTime Date { get; set; }
+        public virtual Manager Manager { get; set; }
+        public virtual Client ClientId { get; set; }
+        public double Cost { get; set; }
+    }
+}
--- DbAutoActService/DbAutoActService/Program.cs
+++ DbAutoActService/DbAutoActService/Program.cs
@@ -24,24 +24,25 @@ namespace DbAutoActService
                 new DbAutoActualizationService() 
             };
 
-            if(Environment.UserInteractive)
-            {
-                Type type = typeof(ServiceBase);
-                BindingFlags flags = BindingFlags.Instance | BindingFlags.NonPublic;
-                MethodInfo method = type.GetMethod("OnStart", flags);
-                foreach (ServiceBase service in ServicesToRun)
-                {
-                    method.Invoke(service, new object[] { args });
-                }
-                Console.WriteLine("Press any key to exit");
-                Console.ReadLine();
-                MethodInfo methodStop = type.GetMethod("OnStop", flags);
-                foreach (ServiceBase service in ServicesToRun)
-                {
-                methodStop.Invoke(service, null);
-                }
-            }
-            else ServiceBase.Run(ServicesToRun); 
+            //if(Environment.UserInteractive)
+            //{
+            //    Type type = typeof(ServiceBase);
+            //    BindingFlags flags = BindingFlags.Instance | BindingFlags.NonPublic;
+            //    MethodInfo method = type.GetMethod("OnStart", flags);
+            //    foreach (ServiceBase service in ServicesToRun)
+            //    {
+            //        method.Invoke(service, new object[] { args });
+            //    }
+            //    Console.WriteLine("Press any key to exit");
+            //    Console.ReadLine();
+            //    MethodInfo methodStop = type.GetMethod("OnStop", flags);
+            //    foreach (ServiceBase service in ServicesToRun)
+            //    {
+            //    methodStop.Invoke(service, null);
+            //    }
+            //}
+            //else 
+            ServiceBase.Run(ServicesToRun); 
         }
     }
 }
